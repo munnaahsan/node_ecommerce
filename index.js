31a181793cd89a881cee033a07009c79802848d1
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2021-05-31 14:43:30 +0600

    Testing and dependencies and boilerpalate added using chai & mocha

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express();
 
-app.get('/', function(req,res) {
+app.get('/', (req,res) => {
     return res.send("Hello Munna");
 })
 
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log("Server Runnning on Port 30000");
 })
+
+
+module.exports = app
