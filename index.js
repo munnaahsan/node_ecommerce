f9a3cdd0ce752a71c5e93c9a0dfa024ce140a095
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2021-06-10 17:57:39 +0600

    Registration Page

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const packageName = require('./models/User');
+
+require('./utils/db.config'); 
 const app = express()
-require('./utils/db.config');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 
 app.set('view engine', 'ejs')
 
@@ -8,6 +16,12 @@ app.get('/', (req, res) => {
   return res.render('index')
 })
 
+app.get('/register', (req, res) => {
+  return res.render('register')
+})
+app.post('/register', (req,res) => {
+  return res.send(req.body)
+})
 app.listen(3000, () => {
   console.log('Server Runnning on Port 30000')
 })
