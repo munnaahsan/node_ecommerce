2346ef86332a97d9a640250750ed7f3f3d39a55d
Munna Ahsan <54517912+munnaahsan@users.noreply.github.com>
2021-06-13 17:26:24 +0600

    validation

--- modules/users/service/userService.js
+++ modules/users/service/userService.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 
+
 /**
  * Create a new user and return it
  * @param {object} userInput it is user input with all variables for user model
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
+
 const router = express.Router();
 
 const { addUser } = require('../modules/users/service/userService');
+const { registerSchema } = require('../modules/users/validations/authValidation')
 
 
 /**
@@ -13,18 +15,27 @@ router.get('/register', (req,res) => {
 })
 
 
+
 /**
  * Handles user registration
  */
 
-
  router.post('/register', async (req,res) => {
    try {
+     const validationResult = registerSchema.validate(req.body, {
+       abortEarly: false
+     })
+
+     if(validationResult.error) {
+      return res.render('register', {message: "Validation Error"});
+     }
+
     const user = await addUser(req.body)
+    // return res.send(validationResult)
     return res.render('register', {message: "Registration Successfully"});
    } catch (e) {
+    console.log(e);
     return res.status(400).render('register', {message: "Something Went Wrong"});
-     console.log(e);
    }
    
 })
